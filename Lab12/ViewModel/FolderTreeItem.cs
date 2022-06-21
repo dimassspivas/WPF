8f791e1cf9ac8b3d8255cfa9bc94748fb961f9f5
dimassspivas <77319264+dimassspivas@users.noreply.github.com>
2022-06-21 19:40:37 +0300

    Add files via upload

---
+++ Lab12/ViewModel/FolderTreeItem.cs
@@ -0,0 +1,73 @@
+﻿using SharedResources;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileBrowser.ViewModel
+{
+    [Serializable]
+    public class FolderTreeItem: AbstractObservableViewModel
+    {
+        public FolderTreeItem(string name, bool isDrive = false, bool isFloppy = false, bool isCD = false)
+        {
+            Name = name;
+            IsDrive = isDrive;
+            IsFloppy = isFloppy;
+            IsCD = isCD;
+        }
+
+        public string Name { get; }
+        
+        public string ShortName
+        { 
+            get
+            {
+                var result = Path.GetFileName(Name);
+                if (result.Length == 0 || result == "")
+                {
+                    return Name;
+                }
+                return result;
+            }
+        }
+
+        public bool IsFolder { get => !IsDrive && !IsFloppy && !IsCD;  }
+        
+        public bool IsDrive { get; private set; }
+        
+        public bool IsFloppy { get; private set; }
+        
+        public bool IsCD { get; private set; }
+        
+        public bool Expanded { get; set; } = false;
+
+        public string ImageUri
+        {
+            get
+            {
+                const string resourceRoot = "pack://application:,,,/Resources/tree/{0}";
+                var icon = "";
+                if (IsFolder)
+                {
+                    icon = Expanded ? "FolderOpened_16x.png" : "FolderClosed_16x.png";
+                    
+                } else if (IsFloppy)
+                {
+                    icon = "FloppyDrive_16x.png";
+                }
+                else if (IsCD)
+                {
+                    icon = "CD_16x.png";
+                } else if (IsDrive)
+                {
+                    icon = "FloppyDrive_16x.png";
+                }
+
+                return string.Format(resourceRoot, icon);
+            }
+        }
+    }
+}
---
+++ Lab12/ViewModel/MainViewModel.cs
@@ -0,0 +1,61 @@
+﻿using SharedResources;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FileBrowser.ViewModel
+{
+    public class MainViewModel: AbstractObservableViewModel
+    {
+        public MainViewModel()
+        {
+            LeftContent = new TreeViewModel<FolderTreeItem>();
+            InitLeftContent();
+        }
+
+        public TreeViewModel<FolderTreeItem> LeftContent { get; private set; }
+
+        private void InitLeftContent()
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (var driveInfo in drives)
+            {
+                LeftContent.Add(
+                    new FolderTreeItem(
+                        driveInfo.Name, 
+                        driveInfo.DriveType == System.IO.DriveType.Fixed,
+                        driveInfo.DriveType == System.IO.DriveType.Removable,
+                        driveInfo.DriveType == System.IO.DriveType.CDRom
+                    ),
+                    GetDriveDirectories(driveInfo.Name)
+                );
+            }
+        }
+
+        private ObservableCollection<TreeItemViewModel<FolderTreeItem>> GetDriveDirectories(string path)
+        {
+            ObservableCollection<TreeItemViewModel<FolderTreeItem>> result = new ObservableCollection<TreeItemViewModel<FolderTreeItem>>();
+            try
+            {
+                foreach (var dir in Directory.GetDirectories(path))
+                {
+                    result.Add(
+                        new TreeItemViewModel<FolderTreeItem>() { Data = new FolderTreeItem(dir) }
+                    );
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            
+            return result;
+        }
+
+    }
+}
