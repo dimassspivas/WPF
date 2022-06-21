00cd6975f0bc6c2941ef1b3163b926b252cff90e
dimassspivas <77319264+dimassspivas@users.noreply.github.com>
2022-06-21 19:30:58 +0300

    Add files via upload

---
+++ Lab3/Downloader.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Lab3
+{
+    public class Downloader
+    {
+        public Uri Address { get; set; }
+        public WebClient WebClient { get; set; }
+        public DownloadProgressChangedEventHandler ProgressChanged;
+        public string FileName { get; set; }
+
+        public  Downloader(string address, DownloadProgressChangedEventHandler handler)
+        {
+            WebClient = new WebClient();
+            Address = new Uri(address);
+            ProgressChanged = handler;
+            
+        }
+        public async Task<bool> DownloadFileAsync()
+        {
+            await Task.Run(() => {
+                WebClient.DownloadProgressChanged += ProgressChanged;
+                WebClient.DownloadFileAsync(Address, FileName);
+            });
+            return true;
+        }
+        public void StopDownload()
+        {
+            WebClient.CancelAsync();
+        }
+        public string GetInfo()
+        {
+            
+            using (WebClient = new WebClient()) 
+            {
+                
+                try
+                {
+                    WebClient.OpenRead(Address);
+                    var header_contentDisposition = WebClient.ResponseHeaders["content-disposition"];
+
+                    string filename = new ContentDisposition(header_contentDisposition).FileName;
+                    return filename;
+                }
+                catch
+                {
+                    return "file.dat";
+                }
+
+                
+            }
+            
+        }
+        public async Task<bool> DownloadPageAsync()
+        {
+            await Task.Run(() =>
+            {
+                string html = WebClient.DownloadString(Address);
+                File.WriteAllText(FileName, html, Encoding.UTF8);
+            });
+            return true;
+        }
+        
+        
+    }
+}
---
+++ Lab3/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Net;
+using System.Threading;
+
+namespace Lab3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Downloader downloader;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TxtLink_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Regex urlRegex = new Regex(@"^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$");
+            if (urlRegex.IsMatch(TxtLink.Text))
+                DownloadStart.IsEnabled = true;
+            else
+                DownloadStart.IsEnabled = false;
+        }
+        public void ProgressBarUpdate(object sender, DownloadProgressChangedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                PrBar.Value = e.ProgressPercentage;
+                TxtPercent.Text = $"Downloaded: {e.ProgressPercentage.ToString()}%";
+                TxtSize.Text = $"{(long)e.BytesReceived/ 1048576} / {e.TotalBytesToReceive/ 1048576} MB" ;
+
+            });
+        }
+
+        private async void DownloadStart_Click_Start(object sender, RoutedEventArgs e)
+        {
+            DownloadStart.IsEnabled = false;
+            DownloadStop.IsEnabled = true;
+            downloader = new Downloader(TxtLink.Text, ProgressBarUpdate);
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            if (ComboBoxType.SelectedIndex == 0)
+            {
+                sfd.FileName = downloader.GetInfo();
+                
+                if (sfd.ShowDialog().Value)
+                {
+                    downloader.FileName = sfd.FileName;
+                    Path.Text = $"Path to download: {sfd.FileName}";
+                    bool t = await downloader.DownloadFileAsync();
+                    if (t)
+                    {
+                        DownloadStart.IsEnabled = true;
+                    }
+                }
+                else
+                {
+                    DownloadStart.IsEnabled = true;
+                }
+               
+
+            }
+            else if (ComboBoxType.SelectedIndex == 1)
+            {
+                sfd.FileName = "index.html";
+                if (sfd.ShowDialog().Value)
+                {
+                    downloader.FileName = sfd.FileName;
+                    Path.Text = $"Path to download: {sfd.FileName}";
+                    if (await downloader.DownloadPageAsync())
+                    {
+                        PrBar.Value = 100;
+                        TxtPercent.Text = $"Downloaded: 100%";
+                        DownloadStart.IsEnabled = true;
+                    }
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("Error!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                DownloadStart.IsEnabled = true;
+            }
+           
+
+        }
+
+        private void DownloadStop_Click_Stop(object sender, RoutedEventArgs e)
+        {
+            downloader.StopDownload();
+        }
+    }
+}
